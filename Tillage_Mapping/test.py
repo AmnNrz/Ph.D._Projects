c0fb6491677bceba2bfe660e10c515b167088f77
AmnNrz <aminnorouzi@Amins-MacBook-Air.local>
2023-09-06 15:15:50 -0700

    _

--- Tillage_Mapping/test.py
+++ Tillage_Mapping/test.py
@@ -1,10 +1,21 @@
-#!/usr/bin/env python
-# coding: utf-8
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.15.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
 
-# In[ ]:
-
-
+# %%
 a = 1
 g = 4
 asdf = 4
-
+f= 5
+g = 1
