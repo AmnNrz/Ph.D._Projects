4af9ce8a101833a0503f3f6e040a116163d0bf73
Amin Norouzi <amnnrz@PACCAR.PC>
2024-07-18 15:47:52 -0700

    /

--- Codes/main/residue_epo.R
+++ Codes/main/residue_epo.R
@@ -2,8 +2,6 @@ setwd(paste0('/home/amnnrz/Documents/GitHub/spectroscopy_paper/Codes/main/'))
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
-library(patchwork)
-library(viridis)
 source("epo_module_new.R")
 
 
@@ -11,131 +9,147 @@ source("epo_module_new.R")
 path_to_data <- paste0('/home/amnnrz/OneDrive - a.norouzikandelati/Ph.D/',
                        'Projects/Soil_Residue_Spectroscopy/Data/10nm_resolution/')
 
-residue_df <- read.csv(paste0(path_to_data,
+residue_df <- read.csv(paste0(path_to_data, 
                                   "Residue.csv"),
                            header = TRUE, row.names = NULL)
-
-# Remove 'scan' while keeping all other columns
-
-
 residue_df <- residue_df[-c(1, 8)]
-residue_df <- subset(residue_df, select = -Scan)
 
-residue_df <- rename(residue_df, Crop = Soil)
+residue_df <- rename(residue_df, Crop = Soil)  
 
 residue_df <- residue_df %>%
   rename(Type = Crop)
-
-
-
-
-
-library(tibble)
-library(reshape2)
-library(tidyverse)
-library(dplyr)
-library(ggplot2)
-
-
-type <- "Weathered Wheat"
-Dataframe <- residue_df
-# EPO function
-
-
-
-
-library(tibble)
-library(reshape2)
-library(tidyverse)
-library(dplyr)
-library(ggplot2)
+  
 
 
 # epo
-residue_transformed <- epo(residue_df)
+type <- unique(residue_df$Type)[5]
+num_pc = 2
+Dataframe <- residue_df
+epo <- function(Dataframe, num_pc = 2){
+  transformed_DF <- data.frame()
+  for (type in unique(Dataframe$Type)){
+    df <- dplyr::filter(Dataframe, Type==type)
+    df <- df %>% distinct(Wvl, RWC, .keep_all = TRUE)
+    df <- df %>% select(-Scan)
+    df <- df %>% 
+      pivot_wider(names_from = RWC, values_from = Reflect) 
+    
+    df <- as.data.frame(df)
+    rownames(df) <- df$Wvl
+    
+    X <- df[, 4:ncol(df)]
+    # Convert column names to numeric and order the columns
+    X <- X[, order(as.numeric(names(X)))]
+    X <- X[, ncol(X):1]
+    
+    min_col <- which.min(colnames(X))
+    X_wet <- X[,-min_col]
+    
+    D <- X_wet - matrix(rep(X[,min_col], ncol(X_wet)),
+                        ncol = ncol(X_wet), byrow = FALSE)
+    
+    D_mat <- as.matrix(D)
+    # D_mat <- -D_mat
+    
+    svd_result <- svd(D_mat)
+    # svd_result <- svd(D_mat %*% t(D_mat))
+
+    
+    # U matrix (left singular vectors)
+    U <- svd_result$u
+    
+    # Sigma (diagonal matrix of singular values)
+    Sigma <- svd_result$d
+    
+    # Compute the rank of A based on non-zero singular values
+    r <- sum(Sigma > 1e-10)  # Threshold to avoid numerical issues
+    
+    # Compute the projection matrix P using the first r columns of U
+    Q <- U[, 1:r] %*% t(U[, 1:r])
+    
+    # Print the projection matrix P
+    print(Q)
+ 
+    # P <- diag(nrow(Q)) - Q
+    P <- matrix(1, nrow = nrow(Q), ncol = nrow(Q)) - Q
+    
+    X_wet <- as.matrix(X_wet)
+    P <- as.matrix(P)
+    
+    X_transformed <- X_wet %*% P
+    # X_transformed <- t(X_wet) %*% P
+    # X_transformed <- t(X_transformed)
+    X <- as.data.frame(X)
+    X_transformed <- as.data.frame(X_transformed)
+    row.names(X_transformed) <- row.names(X)
+    colnames(X_transformed) <- colnames(X_wet)
+    X_transformed <- rownames_to_column(X_transformed, var = 'Wvl')
+    X_transformed <- melt(X_transformed, id.vars = "Wvl", variable.name = "RWC",
+                          value.name = "Reflect")
+    org_df_subset <- subset(Dataframe, Type == type)
+    org_df_subset$Scan_RWC <- paste(
+      as.character(org_df_subset$Scan), as.character(org_df_subset$RWC),
+      sep = "_")
+    scan_list <- unique(org_df_subset$Scan_RWC)
+    
+    
+    
+    mapping_list <- strsplit(scan_list, "_")
+    mapping_df <- do.call(rbind, lapply(
+      mapping_list, function(x) data.frame(Scan = x[1], RWC = as.numeric(x[2]))))
+    
+    mapping_dict <- setNames(mapping_df$Scan, mapping_df$RWC)
+    
+    
+    X_transformed$Scan <- mapping_dict[as.character(X_transformed$RWC)]
+    
+    
+    X_transformed <- X_transformed %>% select(Scan, everything())
+    
+    X_transformed$Type <- type
+    
+    X_transformed$Sample <- Dataframe$Sample[1]
+    
+    transformed_DF <- rbind(transformed_DF, X_transformed)
+  }
+  return(transformed_DF)
+}
 
 
-# Plot the Spectra before and after epo
-residue_df$source <- 'Original'
-residue_transformed$source <- 'EPO'
-
-df_combined <- rbind(residue_df, residue_transformed)
-df_combined
-
-df_combined$RWC <- as.numeric(as.character(df_combined$RWC))
-
-crp <- "Weathered Wheat"
-
-df_toplot <- dplyr::filter(df_combined, Type == crp)
-
-df_toplot$source <- factor(df_toplot$source, levels = c("Original", "EPO"))
 
 
-library(scales)
-library(RColorBrewer)
-# Define the number of colors you want from the palette
-num_colors <- 11  # Typically, ColorBrewer palettes have up to 9 or 11 distinct colors for the best granularity
-
-# Get the 'Spectral' palette colors and reverse them
-spectral_colors <- brewer.pal(num_colors, "Spectral")
-reversed_colors <- spectral_colors
 
 
-plot <- ggplot(df_toplot, aes(x = Wvl, y = Reflect, group = RWC)) + 
-  geom_line(aes(color = RWC), size = 0.18) + 
-  facet_wrap(~ source, ncol=2, scales = "free_y") +  
-  scale_color_gradientn(colors = reversed_colors) + 
-  labs(
-    title = "Reflectance ~ Wavelength",
-    x = "Wavelength", y = "Reflectance",
-    color = "RWC"
-  ) +
+
+
+
+M <- Q
+#plot heatmap of P
+row.names(M) <- unique(Residue$Wvl)
+colnames(M) <- unique(Residue$Wvl)
+
+# Step 1: Load libraries
+library(ggplot2)
+library(reshape2)
+
+# Assuming 'P' is your matrix and already loaded
+# Step 2: Prepare data (if your matrix is named 'P')
+data_long <- melt(M)
+names(data_long) <- c("X", "Y", "Value")
+
+# Corrected and refined breakpoints
+breakpoints_Ps <- c(-0.0255038, -0.0073811, -0.0046840, -0.0021710, 0.0001066, 0.1, 0.5, 0.9963112)
+breakpoints_Pr <- c(-0.0139223, -0.0042455, -0.0023056, 0.0004914, -0.0001011, 0.9995316 )
+
+breakpoints <- breakpoints_Pr
+# Creating the heatmap with the updated breakpoints
+ggplot(data_long, aes(X, Y, fill = Value)) +
+  geom_tile() +
+  scale_fill_gradientn(colors = colorRampPalette(c("white", "blue", "red", "green"))(length(breakpoints) - 1),
+                       values = scales::rescale(breakpoints)) +
+  scale_x_continuous(breaks = seq(500, 2450, by = 400)) +
+  scale_y_continuous(breaks = seq(500, 2450, by = 400)) +
   theme_minimal() +
-  theme(plot.title = element_text(hjust = 0.5),
-        panel.grid.major = element_blank(),        
-        panel.grid.minor = element_blank(),       
-        panel.background = element_rect(fill = "white"),
-        axis.ticks = element_line(color = "black"),
-        strip.background = element_rect(fill = "white"),
-        plot.background = element_rect(fill = "white")
-  )
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(fill = "Value", x = "Pr", y = "")
 
-print(plot)
-
-
-
-
-# # # Plot Q/P matrix
-# M  <-  P
-# #plot heatmap of P
-# row.names(M) <- unique(residue_df$Wvl)
-# colnames(M) <- unique(residue_df$Wvl)
-# 
-# # Step 1: Load libraries
-# library(ggplot2)
-# library(reshape2)
-# 
-# # Assuming 'P' is your matrix and already loaded
-# # Step 2: Prepare data (if your matrix is named 'P')
-# data_long <- melt(M)
-# names(data_long) <- c("X", "Y", "Value")
-# 
-# # Assume data_long is your dataframe and Value is the column you're visualizing
-# quantiles <- quantile(data_long$Value, probs = seq(0, 1, by = 0.1))  # Adjust sequence as needed
-# # Creating breakpoints from quantiles
-# breakpoints <- c(min(data_long$Value), quantiles, max(data_long$Value))
-# 
-# # Creating the heatmap with defined breakpoints
-# ggplot(data_long, aes(x = X, y = Y, fill = Value)) +
-#   geom_tile() +
-#   scale_fill_gradientn(colors = colorRampPalette(c("white", "blue", "red", "green"))(length(breakpoints) - 1),
-#                        values = rescale(breakpoints)) +
-#   scale_x_continuous(breaks = seq(500, 2450, by = 400)) +
-#   scale_y_continuous(breaks = seq(500, 2450, by = 400)) +
-#   theme_minimal() +
-#   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-#   labs(fill = "Value", x = "Pr", y = "")
-# 
-# 
-# 
-# 
---
+++ Codes/main/story_figs.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(viridis)
+
+
+path_to_data <- paste0('/home/amnnrz/OneDrive - a.norouzikandelati/Ph.D/',
+                       'Projects/Soil_Residue_Spectroscopy/Data/10nm_resolution/')
+
+# path_to_plots <- paste0('/home/amnnrz/OneDrive - a.norouzikandelati/Ph.D/',
+#                         'Projects/Soil_Residue_Spectroscopy/Plots/10nm_resolution/Wangetal2')
+
+residue = read.csv("")
